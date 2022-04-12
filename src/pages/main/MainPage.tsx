b21162e12bdce2be6393401617c110766eb0d18c
Oleksandra Polenova <d21121972@gmail.com>
2022-04-12 20:05:31 +0300

    add chakra

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,16 +1,19 @@
 import React, { FC } from 'react';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
 import {
-  Header, RandomPlanet
+  Header,
+  RandomPlanet
 } from '../../components';
 import { PeopleContainer } from '../../containers';
 
 const MainPage: FC = () => (
-  <>
+  <ChakraProvider>
     <Header />
     <RandomPlanet />
     <PeopleContainer />
-
-  </>
+  </ChakraProvider>
 );
 
 export default MainPage;
